03dade725d62d7b9d053a1590b2f2660ccd97801
Yuan <49439486+Sky-And-Ground@users.noreply.github.com>
2025-11-28 14:22:09 +0800

    Update test.c

--- test.c
+++ test.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "raylib.h"
 #include "raylib_extend_text_buffer_pool.h"
 
@@ -5,9 +6,18 @@ int main(void)
 {
     struct TextBufferPool pool;
     struct TextData* text;
+    const char* fontPath = "simfang.ttf";
+
+    FILE* f = fopen(fontPath, "r");
+    if (!f) {
+        fprintf(stderr, "cannot found: %s\n", fontPath);
+        return 1;
+    }
+
+    fclose(f);
 
     /* load the simfang.ttf, then we would render text with it. */
-    text_buffer_pool_init(&pool, 256, "simfang.ttf");
+    text_buffer_pool_init(&pool, 256, fontPath);
     InitWindow(800, 450, "测试中文显示");
     
     /* so here, we create our needed text. */
