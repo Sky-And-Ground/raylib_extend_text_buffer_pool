cea77150fc1e9817c2b4a5d44a7e778881e09dbc
Yuan <49439486+Sky-And-Ground@users.noreply.github.com>
2025-11-28 13:06:50 +0800

    Add files via upload

---
+++ raylib_extend_text_buffer_pool.c
@@ -0,0 +1,150 @@
+#include "raylib_extend_text_buffer_pool.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void text_data_init(struct TextBufferPool* pool, struct TextData* data, const char* text, int fontSize) {
+    int codepointsCount;
+    int* codepoints;
+
+    data->text = text;
+    codepoints = LoadCodepoints(text, &codepointsCount);
+    data->font = LoadFontFromMemory(".ttf", pool->fontFileData, pool->fileDataSize, fontSize, codepoints, codepointsCount);
+    UnloadCodepoints(codepoints);
+}
+
+static unsigned int text_buffer_pool_hash(struct TextBufferPool* pool, const char* key) {
+    unsigned int hashval = 0;
+
+    while (*key) {
+        hashval = 131 * hashval + (unsigned int)(*key - '0');
+        ++key;
+    }
+
+    return hashval % pool->bucketSize;
+}
+
+int text_buffer_pool_init(struct TextBufferPool* pool, int bucketSize, const char* fontPath) {
+    int i;
+
+    pool->bucket = (struct TextData**)malloc(bucketSize * sizeof(struct TextData*));
+    if (pool->bucket == NULL) {
+        return 0;
+    }
+
+    for (i = 0; i < bucketSize; ++i) {
+        pool->bucket[i] = NULL;
+    }
+
+    pool->fontFileData = LoadFileData(fontPath, &(pool->fileDataSize));
+    pool->bucketSize = bucketSize;
+    pool->len = 0;
+    return 1;
+}
+
+void text_buffer_pool_destroy(struct TextBufferPool* pool) {
+    int i;
+    for (i = 0; i < pool->bucketSize; ++i) {
+        struct TextData* cursor = pool->bucket[i];
+        struct TextData* tmp;
+
+        while (cursor) {
+            tmp = cursor->next;
+
+            UnloadFont(cursor->font);
+            free(cursor);
+
+            cursor = tmp;
+        }
+    }
+
+    UnloadFileData(pool->fontFileData);
+}
+
+static struct TextData* text_buffer_pool_find_node(struct TextBufferPool* pool, const char* text) {
+    unsigned int hashval = text_buffer_pool_hash(pool, text);
+    struct TextData* cursor = pool->bucket[hashval];
+    
+    while (cursor) {
+        if (strcmp(text, cursor->text) == 0) {
+            return cursor;
+        }
+
+        cursor = cursor->next;
+    }
+    
+    return NULL;
+}
+
+struct TextData* text_buffer_pool_put(struct TextBufferPool* pool, const char* text, int fontSize) {
+    struct TextData* node = text_buffer_pool_find_node(pool, text);
+
+    /* if exists, then update it. */
+    if (node) {
+        UnloadFont(node->font);
+        text_data_init(pool, node, text, fontSize);
+        return node;
+    }
+    else {
+        node = (struct TextData*)malloc(sizeof(struct TextData));
+
+        if (node == NULL) {
+            return NULL;
+        }
+        else {
+            unsigned int hashval = text_buffer_pool_hash(pool, text);
+            text_data_init(pool, node, text, fontSize);
+            
+            if (pool->bucket[hashval] == NULL) {
+                pool->bucket[hashval] = node;
+            }
+            else {
+                node->next = pool->bucket[hashval];
+                pool->bucket[hashval] = node;
+            }
+            
+            pool->len += 1;
+            return node;
+        }
+    }
+}
+
+void text_buffer_pool_del(struct TextBufferPool* pool, const char* text) {
+    unsigned int hashval = text_buffer_pool_hash(pool, text);
+    struct TextData* cursor = pool->bucket[hashval];
+    struct TextData* parent = NULL;
+    
+    while (cursor) {
+        if (strcmp(text, cursor->text) == 0) {
+            if (cursor == pool->bucket[hashval]) {
+                struct TextData* tmp = cursor->next;
+                free(cursor);
+                pool->bucket[hashval] = tmp;
+            }
+            else {
+                parent->next = cursor->next;
+                free(cursor);
+            }
+
+            pool->len -= 1;
+            return;
+        }
+
+        parent = cursor;
+        cursor = cursor->next;
+    }
+}
+
+void text_buffer_pool_seek_text(const struct TextBufferPool* pool) {
+    int i;
+    struct TextData* cursor;
+
+    for (i = 0; i < pool->bucketSize; ++i) {
+        cursor = pool->bucket[i];
+
+        while (cursor) {
+            printf("%s\n", cursor->text);
+            cursor = cursor->next;
+        }
+    }
+}
---
+++ raylib_extend_text_buffer_pool.h
@@ -0,0 +1,57 @@
+/*
+    @author yuan
+    @brief This is my solution to handle some Chinese characters shown problems for raylib, just a cache pool base on a hash map.
+*/
+#ifndef __RAYLIB_EXTEND_TEXT_BUFFER_POOL_H__
+#define __RAYLIB_EXTEND_TEXT_BUFFER_POOL_H__
+
+#include <stddef.h>
+#include <raylib.h>
+
+struct TextData {
+    const char* text;
+    Font font;
+
+    struct TextData* next;
+};
+
+struct TextBufferPool {
+    struct TextData** bucket;
+    unsigned int fileDataSize;
+    unsigned char* fontFileData;
+    size_t bucketSize;
+    size_t len;
+};
+
+/*
+    init the whole thing with your given font.
+    you must make sure that the fontPath is valid, this function won't check that.
+
+    if out of memory, this funtion return 0, else return 1.
+*/
+int text_buffer_pool_init(struct TextBufferPool* pool, int bucketSize, const char* fontPath);
+
+/*
+    destroy the whole thing.
+*/
+void text_buffer_pool_destroy(struct TextBufferPool* pool);
+
+/*
+    build the text data, and return it to you.
+    if the text has been built before, this function would just update it.
+
+    if out of memory, this function return NULL, else return the built data.
+*/
+struct TextData* text_buffer_pool_put(struct TextBufferPool* pool, const char* text, int fontSize);
+
+/*
+    delete one text data by the given name.
+*/
+void text_buffer_pool_del(struct TextBufferPool* pool, const char* text);
+
+/*
+    see what we have already.
+*/
+void text_buffer_pool_seek_text(const struct TextBufferPool* pool);
+
+#endif
