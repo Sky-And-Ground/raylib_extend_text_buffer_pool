97900cb1e3bb2501994d8c06f12b2d702bc6fc00
Yuan <49439486+Sky-And-Ground@users.noreply.github.com>
2025-12-10 17:52:10 +0800

    Update raylib_text_font_binder_test.cpp

--- raylib_text_font_binder_test.cpp
+++ raylib_text_font_binder_test.cpp
@@ -10,13 +10,18 @@ int main(void)
     InitWindow(800, 450, "测试中文显示");
     SetTargetFPS(60);
 
-    TextData data{ "纯是一段这文测试本的 ,", ffd };  // render our needed characters, be cautions here, we also include a space and a comma.
+    // render our needed characters, using wide string.
+    // and be cautions here, we also include a space and a comma.
+    TextData data;
+    data.add_if_not_exists("这是一段文本", ffd);
+    data.add_if_not_exists("纯文本用于测试", ffd);
+    data.add_if_not_exists("是的, 测试专用", ffd);
 
     while (!WindowShouldClose()) 
     {
         BeginDrawing();
         ClearBackground(WHITE);
-        DrawTextEx(*(data.font()), "这是一段文本, 是的, 测试文本", (Vector2){ 50, 50 }, 32, 0, RED);  /* draw it. */
+        DrawTextEx(*(data.font()), "这是一段文本, 专用于测试", (Vector2){ 50, 50 }, 32, 0, RED);  // draw it.
         EndDrawing();
     }
 
