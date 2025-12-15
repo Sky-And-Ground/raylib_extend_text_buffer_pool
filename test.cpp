97764fde4704835ba2f3114ebb3fd758d2fe410f
Yuan <49439486+Sky-And-Ground@users.noreply.github.com>
2025-12-15 10:05:27 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -10,8 +10,7 @@ int main(void)
     InitWindow(800, 450, "测试中文显示");
     SetTargetFPS(60);
 
-    // render our needed characters, using wide string.
-    // and be cautions here, we also include a space and a comma.
+    // render our needed characters. and be cautions here, we also include a space and a comma.
     TextData data;
     data.add_if_not_exists("这是一段文本", ffd);
     data.add_if_not_exists("纯文本用于测试", ffd);
@@ -21,9 +20,11 @@ int main(void)
     {
         BeginDrawing();
         ClearBackground(WHITE);
-        DrawTextEx(*(data.font()), "这是一段文本, 专用于测试", (Vector2){ 50, 50 }, 32, 0, RED);  // draw it.
+        DrawTextEx(data.font().value(), "这是一段文本, 专用于测试", (Vector2){ 50, 50 }, 32, 0, RED);  // draw it.
         EndDrawing();
     }
 
     return 0;
 }
+
+
